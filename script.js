ea24fd2bb21da19862ef6af1622b8cdb8fafdd5e
SAsuke019 <hugo.019@hotmail.com>
2019-10-27 16:45:52 -0300

    site basico

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudar() {
+
+    document.getElementById("foto").src = "img/h.png"
+    document.getElementById("foto2").src = "img/b.svg"
+}
+
+function mudar2() {
+
+    document.getElementById("foto").src = "img/hu.jpg"
+    document.getElementById("foto2").src = ""
+}
