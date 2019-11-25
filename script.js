88366b6ae353cf813bd814e3374b6b66cda4cc56
Hugo Erico <hugo.019@hotmail.com>
2019-11-25 15:44:35 -0300

    atualização site

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function mudar() {
+function aparecer() {
+    if (document.getElementById("nav").style.display == "block") { document.getElementById("nav").style.display = "none"; } else { document.getElementById("nav").style.display = "block"; }
 
-    document.getElementById("foto").src = "img/h.jpg"
-    document.getElementById("foto2").src = "img/b.svg"
-}
+    if (document.getElementById("article").style.display == "none") { document.getElementById("article").style.display = "block"; } else { document.getElementById("article").style.display = "none"; }
 
-function mudar2() {
+    if (document.getElementById("footer").style.position == "fixed") { document.getElementById("footer").style.position = "relative"; } else { document.getElementById("footer").style.position = "fixed"; }
 
-    document.getElementById("foto").src = "img/hu.jpg"
-    document.getElementById("foto2").src = ""
+    if (document.getElementById("img_menu").style.display == "none") { document.getElementById("img_menu").style.display = "block"; } else { document.getElementById("img_menu").style.display = "none"; }
+
+    if (document.getElementById("img_menu1").style.display == "block") { document.getElementById("img_menu1").style.display = "none"; } else { document.getElementById("img_menu1").style.display = "block"; }
 }
